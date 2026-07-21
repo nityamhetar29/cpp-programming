a53c913156a336f7ef14fbbb5aa55caddcf3f127
nityamhetar29 <nityamhetar753@gmail.com>
2026-07-21 10:51:48 +0530

    Add files via upload

---
+++ large.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+ int num1,num2;
+ cout<<"enter first number:";
+ cin>>num1;
+ cout<<"enter second number:";
+ cin>>num2;
+ if (num1>num2)
+ {
+ cout<<"largest number="<<num1<<endl;
+ }
+ else
+ {
+ cout<<"largest number="<<num2<<endl;
+ }
+ return 0;
+}
---
+++ positive.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"enter a number:";
+cin>>num;
+if(num>0)
+{
+cout<<"positive number";
+}
+else if(num<0)
+{
+cout<<"negative number";
+}
+else
+{
+cout<<"number is zero";
+}
+return 0;
+}
