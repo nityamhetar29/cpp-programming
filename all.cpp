7c8869adbf46c92af12e1ff0328143f5b90f2481
nityamhetar29 <nityamhetar753@gmail.com>
2026-07-21 11:01:00 +0530

    Add files via upload

---
+++ all.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int a,b,sum,sub,mul,div,mod;
+cout<<"Enter first number";
+cin>>a;
+cout<<"Enter second number";
+cin>>b;
+sum=a+b;
+sub=a-b;
+mul=a*b;
+div=a/b;
+mod=a%b;
+cout<<"sum="<<sum;
+cout<<"sub="<<sub;
+cout<<"mul="<<mul;
+cout<<"div="<<div;
+cout<<"mod="<<mod;
+return 0;
+}
+
---
+++ rectangle.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+float length,breadth,area,perimeter;
+cout<<"Enter length:";
+cin>>length;
+cout<<"Enter breadth:";
+cin>>breadth;
+area=length+breadth;
+perimeter=2*(length+breadth);
+cout<<"Area of rectangle="<<area<<endl;
+cout<<"Perimeter of rectangle="<<perimeter<<endl;
+return 0;
+}
